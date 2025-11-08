71d402a37248d9e5f9e1f54c1bee077fb8b77472
SkyRaks <nano.voloshin2016@gmail.com>
2025-11-07 20:12:05 -0500

    now it renders messages on page

--- rtchat/views.py
+++ rtchat/views.py
@@ -5,7 +5,7 @@ from .models import *
 # Create your views here.
 @login_required
 def chat_view(request):
-    # chat_group = get_object_or_404(ChatGroup, group_name='public-chat')
-    # chat_messages = chat_group.chat_messages.all()[:30]
+    chat_group = get_object_or_404(ChatGroup, group_name='public')
+    chat_messages = chat_group.chat_messages.all()[:30]
     # {'chat_messages': chat_messages}
-    return render(request, 'rtchat/chat.html')
+    return render(request, 'rtchat/chat.html', {'chat_messages': chat_messages})
